272a4c5ecda7d087d1f55b43054c4e51e2615ee5
= <=>
2023-10-16 15:20:57 +0300

    com

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-const path = require("path");
+// const path = require("path");
 // console.log(path.parse(__filename).ext);
-const fs = require("fs");
+// const fs = require("fs");
 // fs.mkdir(path.join(__dirname, "tmp"), (err) => {
 //   if (err) {
 //     console.log(err);
 //   }
 //   console.log("папка создана");
 // });
-const filePath = path.join(__dirname, "tmp", "tmp.txt");
+// const filePath = path.join(__dirname, "tmp", "tmp.txt");
 // fs.writeFile(filePath, "Привет студенты", (err) => {
 //   if (err) {
 //     console.log(err);
 //   }
 //   console.log("Файл создан");
 // });
-fs.appendFile(filePath, "\nДо свидания, студенты", (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("файл changed");
+// fs.appendFile(filePath, "\nДо свидания, студенты", (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log("файл changed");
+// });
+
+const express = require("express");
+const app = express();
+const port = "3000";
+
+app.get("/", (req, res) => {
+  res.end("Welcome");
+});
+
+app.post("/", (req, res) => {});
+
+app.listen(port, function () {
+  console.log("Сервер запущен порт" + port);
 });
